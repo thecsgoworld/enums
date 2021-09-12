c87f51923491df4e5be184d6f6df0e3d176cac7f
derpierre65 <git@derpierre65.de>
2021-09-12 19:57:54 +0200

    update enums

--- src/Enums/User/UserStatistic.php
+++ src/Enums/User/UserStatistic.php
@@ -6,6 +6,6 @@ final class UserStatistic {
 	public const MONEY_COLLECTED = 'money_collected';
 	public const CASES_COLLECTED = 'cases_collected';
 	public const CASES_OPENED = 'cases_opened';
-	public const ITEMS_SOLD_TYPE_VALUE = 'items_sold_value_%d'; // %d = ItemType
-	public const ITEMS_SOLD_TYPE_COUNT = 'items_sold_count_%d'; // %d = ItemType
+	public const ITEMS_SOLD_VALUE = 'items_sold_value%s'; // %s (optional) = _quality[ItemQuality] / _type[ItemType]
+	public const ITEMS_SOLD_COUNT = 'items_sold_count%s'; // %s (optional) = _quality[ItemQuality] / _type[ItemType]
 }
