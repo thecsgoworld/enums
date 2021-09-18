a5bfb414dd3e1ff252f82089f312862b87046f78
derpierre65 <git@derpierre65.de>
2021-09-18 19:37:51 +0200

    add JackpotStatus enum

---
+++ src/Enums/Jackpot/JackpotStatus.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace TheCSGOWorld\Enums\Jackpot;
+
+final class JackpotStatus {
+	public const RUNNING = 1;
+	public const STARTING = 2;
+	public const STARTED = 3;
+	public const FINISHED = 4;
+}
