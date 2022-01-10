80e135e91b5f976eaa6d0785fd5112127810b3cb
derpierre65 <git@derpierre65.de>
2022-01-10 21:18:16 +0100

    added more statistics

--- src/Enums/User/UserStatistic.php
+++ src/Enums/User/UserStatistic.php
@@ -8,4 +8,12 @@ final class UserStatistic {
 	public const CASES_OPENED = 'cases_opened';
 	public const ITEMS_SOLD_VALUE = 'items_sold_value%s'; // %s (optional) = _quality[ItemQuality] / _type[ItemType]
 	public const ITEMS_SOLD_COUNT = 'items_sold_count%s'; // %s (optional) = _quality[ItemQuality] / _type[ItemType]
+
+	// geld, contracts, opened cases, missions finished, case battle wins, coin flip wins, jackpot wins, jackpot profit
+	public const MISSIONS_COMPLETED = 'missions_completed';
+	public const CONTRACTS_FINISHED = 'contracts_finished';
+	public const CASE_BATTLE_WINS = 'case_battle_wins';
+	public const COIN_FLIP_WINS = 'coin_flip_wins';
+	public const JACKPOT_WINS = 'jackpot_wins';
+	public const JACKPOT_PROFIT = 'jackpot_profit';
 }
