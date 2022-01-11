c32026ffa105ab60b8af3901c22267ca27729d50
derpierre65 <git@derpierre65.de>
2022-01-11 20:25:54 +0100

    sort statistic enums

--- src/Enums/User/UserStatistic.php
+++ src/Enums/User/UserStatistic.php
@@ -4,18 +4,27 @@ namespace TheCSGOWorld\Enums\User;
 
 final class UserStatistic {
 	// items
+	public const ITEMS_FOUND = 'items_found_%s'; // %s = quality[ItemQuality], stattrak or knives
+	public const CASES_COLLECTED = 'cases_collected';
+	public const CASES_SOLD = 'cases_sold';
 	public const ITEMS_SOLD_VALUE = 'items_sold_value%s'; // %s (optional) = _quality[ItemQuality] / _type[ItemType]
 	public const ITEMS_SOLD_COUNT = 'items_sold_count%s'; // %s (optional) = _quality[ItemQuality] / _type[ItemType]
-	public const ITEMS_FOUND = 'items_found_%s'; // %s = quality[ItemQuality] or stattrak or knives
 
+	// main
 	public const MONEY_COLLECTED = 'money_collected';
-	public const CASES_COLLECTED = 'cases_collected';
-	public const CASES_OPENED = 'cases_opened';
-
+	public const EXPERIENCE_GAINED = 'experience_gained';
 	public const MISSIONS_COMPLETED = 'missions_completed';
-	public const CONTRACTS_FINISHED = 'contracts_finished';
-	public const CASE_BATTLE_WINS = 'case_battle_wins';
+	public const CASES_OPENED = 'cases_opened';
+	public const ITEMS_BOUGHT = 'items_bought';
+	public const KEYS_BOUGHT = 'keys_bought';
+	public const CONTRACTS_SIGNED = 'contracts_signed';
+	public const PRIVATE_RANK_RESETS = 'private_rank_resets';
+
+	// coin flip
 	public const COIN_FLIP_WINS = 'coin_flip_wins';
+
+	// case battle
+	public const CASE_BATTLE_WINS = 'case_battle_wins';
 
 	// jackpot
 	public const JACKPOT_WINS = 'jackpot_wins';
