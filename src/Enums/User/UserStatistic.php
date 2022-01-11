ac9a2ab27c7c6e8cc04bdc3fa11dfc7eee805ad7
derpierre65 <git@derpierre65.de>
2022-01-11 19:41:37 +0100

    more stats

--- src/Enums/User/UserStatistic.php
+++ src/Enums/User/UserStatistic.php
@@ -3,11 +3,14 @@
 namespace TheCSGOWorld\Enums\User;
 
 final class UserStatistic {
+	// items
+	public const ITEMS_SOLD_VALUE = 'items_sold_value%s'; // %s (optional) = _quality[ItemQuality] / _type[ItemType]
+	public const ITEMS_SOLD_COUNT = 'items_sold_count%s'; // %s (optional) = _quality[ItemQuality] / _type[ItemType]
+	public const ITEMS_FOUND = 'items_found_%s'; // %s = quality[ItemQuality] or stattrak or knives
+
 	public const MONEY_COLLECTED = 'money_collected';
 	public const CASES_COLLECTED = 'cases_collected';
 	public const CASES_OPENED = 'cases_opened';
-	public const ITEMS_SOLD_VALUE = 'items_sold_value%s'; // %s (optional) = _quality[ItemQuality] / _type[ItemType]
-	public const ITEMS_SOLD_COUNT = 'items_sold_count%s'; // %s (optional) = _quality[ItemQuality] / _type[ItemType]
 
 	public const MISSIONS_COMPLETED = 'missions_completed';
 	public const CONTRACTS_FINISHED = 'contracts_finished';
@@ -16,6 +19,7 @@ final class UserStatistic {
 
 	// jackpot
 	public const JACKPOT_WINS = 'jackpot_wins';
+	public const JACKPOT_LOST = 'jackpot_lost';
 	public const JACKPOT_PLAYS = 'jackpot_plays';
 	public const JACKPOT_PROFIT = 'jackpot_profit';
 	public const JACKPOT_LOSS = 'jackpot_loss';
