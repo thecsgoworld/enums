e697860eabbd87006dcefd376efe6dc7ce90113f
derpierre65 <git@derpierre65.de>
2022-01-11 18:50:32 +0100

    more jackpot statistics

--- src/Enums/User/UserStatistic.php
+++ src/Enums/User/UserStatistic.php
@@ -9,11 +9,14 @@ final class UserStatistic {
 	public const ITEMS_SOLD_VALUE = 'items_sold_value%s'; // %s (optional) = _quality[ItemQuality] / _type[ItemType]
 	public const ITEMS_SOLD_COUNT = 'items_sold_count%s'; // %s (optional) = _quality[ItemQuality] / _type[ItemType]
 
-	// geld, contracts, opened cases, missions finished, case battle wins, coin flip wins, jackpot wins, jackpot profit
 	public const MISSIONS_COMPLETED = 'missions_completed';
 	public const CONTRACTS_FINISHED = 'contracts_finished';
 	public const CASE_BATTLE_WINS = 'case_battle_wins';
 	public const COIN_FLIP_WINS = 'coin_flip_wins';
+
+	// jackpot
 	public const JACKPOT_WINS = 'jackpot_wins';
+	public const JACKPOT_PLAYS = 'jackpot_plays';
 	public const JACKPOT_PROFIT = 'jackpot_profit';
+	public const JACKPOT_LOSS = 'jackpot_loss';
 }
